c5dbd838e2ee35579b5fc05dfcb62ef510c7c808
Minerr <jack.corp@hotmail.com>
2018-04-26 12:11:27 +0200

    fixed Image description

--- Model/Image.cs
+++ Model/Image.cs
@@ -10,14 +10,14 @@ namespace Model
     {
 		public DateTime TimeAndDate { get; private set; }
         public string GPSLocation { get; private set; }
-		public string Decription { get; private set; }
+		public string Description { get; private set; }
 		public string EmployeeID { get; private set; }
 
-		public Image(DateTime dateTime, string location, string decription, string employeeID)
+		public Image(DateTime dateTime, string location, string description, string employeeID)
         {
 			TimeAndDate = dateTime;
             GPSLocation = location;
-			Decription  = decription;
+			Description  = description;
 			EmployeeID = employeeID;
         }
     }
