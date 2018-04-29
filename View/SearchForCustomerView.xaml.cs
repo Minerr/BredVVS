35eb70242ec181bff190e43fa570ba92b37a8e50
Minerr <jack.corp@hotmail.com>
2018-04-29 14:18:24 +0200

    Changed buttons height to 24. Created SearchForCustomerView Added border to LoginView Made LoginView the main window Removed MainWindow

--- View/SearchForCustomerView.xaml.cs
+++ View/SearchForCustomerView.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace View
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for SearchForClientView.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class SearchForCustomerView : Window
 	{
-		public MainWindow()
+		public SearchForCustomerView()
 		{
 			InitializeComponent();
+		}
+
+		private void SearchForCustomerButton_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
