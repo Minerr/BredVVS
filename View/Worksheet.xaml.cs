9dd661540757c96b4e6f1af4a5407ded6a35e78e
Benjamin Faarvang Grand <benj8147@edu.eal.dk>
2018-05-01 09:27:15 +0200

    Worksheet added

---
+++ View/Worksheet.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void AddHoursButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void CreateTermsheetButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void SaveWorksheetButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
