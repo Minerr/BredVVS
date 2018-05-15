6a6bdd20c9766c09665d72b0bdc87003897fd7a5
Daniel Gierahn <dani881f@edu.eal.dk>
2018-05-15 14:42:35 +0200

    Add'd UC: WorkHours, Termsheet

---
+++ View/UserControls/SearchForCustomerUC.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SearchForCustomerUC.xaml
+    /// </summary>
+    public partial class SearchForCustomerUC : UserControl
+    {
+        private SearchForCustomerViewModel _searchForCustomerVM;
+        public SearchForCustomerUC()
+        {
+            _searchForCustomerVM = new SearchForCustomerViewModel();
+            Init();
+        }
+
+        public SearchForCustomerUC(SearchForCustomerViewModel viewModel)
+        {
+            _searchForCustomerVM = viewModel;
+            Init();
+        }
+
+        private void Init()
+        {
+            InitializeComponent();
+            DataContext = _searchForCustomerVM;
+        }
+    }
+}
---
+++ View/UserControls/TermsheetUC.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TermsheetUC.xaml
+    /// </summary>
+    public partial class TermsheetUC : UserControl
+    {
+        private TermsheetViewModel _termsheetVM;
+		public TermsheetUC()
+		{
+            _termsheetVM = new TermsheetViewModel();
+			Init();
+		}
+
+		public TermsheetUC(TermsheetViewModel viewModel)
+		{
+            _termsheetVM = viewModel;
+			Init();
+		}
+
+		private void Init()
+		{
+			InitializeComponent();
+			DataContext = _termsheetVM;
+		}
+    }
+}
