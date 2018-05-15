2def138046dffc6cd92b0d2f8481716d71b9c7a3
Minerr <jack.corp@hotmail.com>
2018-05-15 09:56:52 +0200

    Added FitterMenu View and ViewModel

---
+++ View/FitterMenuView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace View
+{
+	/// <summary>
+	/// Interaction logic for FitterMenuView.xaml
+	/// </summary>
+	public partial class FitterMenuView : Window
+	{
+		private FitterMenuViewModel _fitterMenuViewModel;
+		public FitterMenuView()
+		{
+			_fitterMenuViewModel = new FitterMenuViewModel();
+			InitializeComponent();
+			DataContext = _fitterMenuViewModel;
+		}
+
+		private void SearchForWorksheetButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
