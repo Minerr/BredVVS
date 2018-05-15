e9eb866807564baf8daf040bc85cdd7d99ad09a2
Minerr <jack.corp@hotmail.com>
2018-05-15 14:56:57 +0200

    Made MainWindow the starturi in App

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using View.UserControls;
 
 namespace View.Windows
 {
@@ -22,6 +23,7 @@ namespace View.Windows
 		public MainWindow()
 		{
 			InitializeComponent();
+			MainGrid.Children.Add(new LoginUC());
 		}
 	}
 }
