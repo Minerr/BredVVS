e6b0834e6ac0abb19c4a18dafc9c7b272bb4b88e
Jacob Lausten Pedersen <jaco728n@edu.eal.dk>
2018-05-15 13:26:56 +0200

    Martins request for fejl kode

--- View/AddTaskView.xaml.cs
+++ View/AddTaskView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace View
 {
@@ -22,6 +23,13 @@ namespace View
 		public AddTaskView()
 		{
 			InitializeComponent();
+			DataContext = AddTaskViewModel.Instance;
+		}
+
+		private void AddTaskButton_Click(object sender, RoutedEventArgs e)
+		{
+			AddTaskViewModel.Instance.SelectTask();
+			this.Close();
 		}
 	}
 }
---
+++ ViewModel/AddTaskViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ViewModel
+{
+	public class AddTaskViewModel
+	{
+		public List<string> TasksList
+		{
+			get
+			{
+				return RetrieveTasks();
+			}
+		}
+
+		public string SelectedTask { get; set; }
+
+		private static AddTaskViewModel _instance;
+
+		public AddTaskViewModel()
+		{
+		}
+
+		public static AddTaskViewModel Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new AddTaskViewModel();
+				}
+
+				return _instance;
+			}
+		}
+
+		public List<string> RetrieveTasks()
+		{
+			List<string> tasks = new List<string>();
+
+			tasks.Add("Installer håndvask");
+			tasks.Add("Installer toilet");
+			tasks.Add("Installer bruser");
+
+			return tasks;
+		}
+
+		public void SelectTask()
+		{
+			TermsheetViewModel.Instance.AddTask(SelectedTask);
+		}
+
+	}
+}
