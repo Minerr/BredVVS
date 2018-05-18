40c9a2e4687f181f13696973fbd2c8ac44588307
Daniel Gierahn <dani881f@edu.eal.dk>
2018-05-18 10:05:10 +0200

    DAL inplementet

---
+++ DataAccess/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DataAccess
+{
+    class CustomerRepository : IRepository<Customer>
+    {
+        public void Create(Customer type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Retrieve(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DataAccess
+{
+    class EmployeeRepository : IRepository<Employee>
+    {
+        public void Create(Employee type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Employee type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Retrieve(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeByCredentials(int ID, string pass)
+        {
+            return;
+        }
+    }
+}
