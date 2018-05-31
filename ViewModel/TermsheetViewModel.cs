23e5908c0e5bca4fde5689271227cb254b88893e
Jacob Lausten Pedersen <jaco728n@edu.eal.dk>
2018-05-31 22:28:06 +0200

    FixedPrice to Estimate OnPropertyChanged

--- ViewModel/TermsheetViewModel.cs
+++ ViewModel/TermsheetViewModel.cs
@@ -180,7 +180,7 @@ namespace ViewModel
 					_termsheetPayment = PaymentType.Estimate;
 				}
 
-				OnPropertyChanged("IsFixedPriceChecked");
+				OnPropertyChanged("IsEstimateChecked");
 				OnPropertyChanged("IsBillChecked");
 				OnPropertyChanged("IsOfferChecked");
 			}
@@ -199,7 +199,7 @@ namespace ViewModel
 					_termsheetPayment = PaymentType.Bill;
 				}
 
-				OnPropertyChanged("IsFixedPriceChecked");
+				OnPropertyChanged("IsEstimateChecked");
 				OnPropertyChanged("IsBillChecked");
 				OnPropertyChanged("IsOfferChecked");
 			}
@@ -218,7 +218,7 @@ namespace ViewModel
 					_termsheetPayment = PaymentType.Offer;
 				}
 
-				OnPropertyChanged("IsFixedPriceChecked");
+				OnPropertyChanged("IsEstimateChecked");
 				OnPropertyChanged("IsBillChecked");
 				OnPropertyChanged("IsOfferChecked");
 			}
